a77100a1265adb30391afbcbf15b2113611f048b
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-10-23 16:29:48 -0400

    securevault

---
+++ index.js
@@ -0,0 +1,22 @@
+//create a string//
+//create an alert saying: "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"//
+const aString = ``;
+alert ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+//Asign variables containing a sum that results in 10, 40, 39//
+const sum = 2 + 2 + 6;
+console.log(sum); //10
+//variable of division for 40//
+const a = 80;
+const b = 2;
+const result = a / b;
+console.log(result); //40
+
+//variable of multiplication for 39//
+const s = 3;
+ d = 13;
+const c = s * d;
+console.log(c); //39
+
+//create a dialog box//
+const aDiaologbox = ``
+alert ("You have unlock the vault, you've been saved by Jesus");
